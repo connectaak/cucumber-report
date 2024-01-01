67bb7481c2cd0a745ee1dbda97f0d293ec22b142
aaklabs101 <aaklabs23101@gmail.com>
2024-01-01 21:00:40 +0600

    file rename and component rename fixed

---
+++ src/containers/DurationSummery.js
@@ -0,0 +1,59 @@
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import PropTypes from "prop-types";
+import * as React from "react";
+import { useState } from "react";
+import { TabPanel } from "../components/TabPanel";
+import TrendChart from "../components/TrendChart";
+import useReportData from "../hooks/useReportData";
+import { a11yProps } from "../utils/a11yProps";
+import { getTrendChartData } from "../utils/getTrendChartData";
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+export default function DurationSummery() {
+  const [value, setValue] = useState(0);
+  const { data } = useReportData();
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const { scenarios, features, steps } = getTrendChartData(data);
+  return (
+    <Box id="trendchart" sx={{ width: "100%" }}>
+      <Box
+        sx={{ marginTop: "20px", display: "flex", justifyContent: "center" }}
+      >
+        <Tabs
+          textColor="red"
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "#FCD73C",
+              TextColor: "red",
+            },
+          }}
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Feature" {...a11yProps(0)} />
+          <Tab label="Scenarios" {...a11yProps(1)} />
+          <Tab label="Steps/Tests" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <TrendChart data={features} title="Feature" />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <TrendChart data={scenarios} title="Scenarios" />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <TrendChart data={steps} title="Steps/Tests" steps />
+      </TabPanel>
+    </Box>
+  );
+}
