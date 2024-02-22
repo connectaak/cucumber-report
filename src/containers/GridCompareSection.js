2db4bea3b6e035dec3a84996b06bb1c990926ed9
aaklabs101 <aaklabs23101@gmail.com>
2024-02-22 20:23:30 +0600

    grid summery completed

--- src/containers/GridCompareSection.js
+++ src/containers/GridCompareSection.js
@@ -9,42 +9,6 @@ import useReportData from "../hooks/useReportData";
 import { getReportCompareGrid } from "../utils/getReportCompareGrid";
 
 const GridCompareSection = () => {
-  const [previousData, setPreviousData] = useState();
-  const { data, customData } = useReportData();
-  const [gridCompareData, setGridCompareData] = useState([]);
-  const handleFileUpload = (event) => {
-    event.preventDefault();
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = async (readerEvent) => {
-      try {
-        // Try to run this code
-        const content = readerEvent.target.result;
-        if (content) {
-          // validateCucumberData(JSON.parse(content));
-          const cucumberJsonObject = JSON.parse(content);
-          //   const cucuberReportObject = {
-          //     data: cucumberJsonObject,
-          //   };
-          //   cucuberReportObject.reportName = file?.name?.split(".")[0];
-          console.log(cucumberJsonObject, "contetn");
-          setPreviousData(cucumberJsonObject);
-        }
-      } catch (err) {
-        // if any error, Code throws the error
-
-        swal({
-          title: "Oops",
-          text: "Please upload a cucumber json file",
-          icon: "warning",
-          button: "ok",
-        });
-      }
-
-      event.target.value = null;
-    };
-  };
   const today = new Date();
   // Get the current date components
   const month = today.getMonth() + 1; // Month is zero-based, so add 1
@@ -60,53 +24,85 @@ const GridCompareSection = () => {
     day +
     "/" +
     year;
+  const { data, customData } = useReportData();
+  const [previousData, setPreviousData] = useState([]);
+  const [gridCompareData, setGridCompareData] = useState([]);
+
+  const handleFileUpload = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = async (readerEvent) => {
+      try {
+        // Try to run this code
+        const content = readerEvent.target.result;
+        if (content) {
+          const cucumberJsonObject = JSON.parse(content);
+
+          console.log(cucumberJsonObject, "contetn");
+          setPreviousData([...previousData, cucumberJsonObject]);
+        }
+      } catch (err) {
+        // if any error, Code throws the error
+
+        swal({
+          title: "Oops",
+          text: "Please upload a cucumber json file",
+          icon: "warning",
+          button: "ok",
+        });
+      }
+
+      event.target.value = null;
+    };
+  };
+
+  useEffect(() => {
+    const customDataJson = { datetime: formattedDate, data: customData };
+    setPreviousData([customDataJson]);
+  }, [customData]);
   // Example usage:
   useEffect(() => {
-    if (customData && previousData) {
-      const compareJson = [
-        previousData,
-        { datetime: formattedDate, data: customData },
-      ];
-      const report = getReportCompareGrid(compareJson);
-
+    if (previousData.length > 1) {
+      const report = getReportCompareGrid(previousData);
       setGridCompareData(report);
     }
   }, [customData, previousData]);
   const classes = useStyles();
   return (
     <Box sx={{ margin: "100px 0" }}>
-      {previousData && data ? (
-        <GridSCompare gridCompareData={gridCompareData} />
-      ) : (
-        <div>
-          <input
-            type="file"
-            id="file_1"
-            name="file"
-            accept=".json"
-            style={{ display: "none" }}
-            onChange={(event) => {
-              handleFileUpload(event);
-            }}
-          />
-          <label className={classes.btnContainer} htmlFor="file_1">
-            <Button
-              className={classes.btnBG}
-              variant="contained"
-              component="span"
-              endIcon={<UploadFileRoundedIcon rounded />}
-            >
-              <Typography sx={{ display: { xs: "none", md: "flex" } }}>
-                Upload Previous JSON
-              </Typography>
-            </Button>
-          </label>
-          <Paper sx={{ padding: "100px 0" }}>
-            <Typography variant="h5" align="center">
-              Please upload previous Json for Grid Comparison!
+      <div>
+        <input
+          type="file"
+          id="file_1"
+          name="file"
+          accept=".json"
+          style={{ display: "none" }}
+          onChange={(event) => {
+            handleFileUpload(event);
+          }}
+        />
+        <label className={classes.btnContainer} htmlFor="file_1">
+          <Button
+            className={classes.btnBG}
+            variant="contained"
+            component="span"
+            endIcon={<UploadFileRoundedIcon rounded />}
+          >
+            <Typography sx={{ display: { xs: "none", md: "flex" } }}>
+              Upload Previous JSON
             </Typography>
-          </Paper>
-        </div>
+          </Button>
+        </label>
+        {/* <Paper sx={{ padding: "100px 0" }}>
+          <Typography variant="h5" align="center">
+            Please upload previous Json for Grid Comparison!
+          </Typography>
+        </Paper> */}
+      </div>
+      {previousData && data && (
+        <GridSCompare gridCompareData={gridCompareData} />
       )}
     </Box>
   );
