fc35bda5073e51b7bb74ecb2d2926260ffd6d782
aaklabs101 <aaklabs23101@gmail.com>
2024-03-08 18:07:20 +0600

    fixed all issues

--- src/containers/GridCompareSection.js
+++ src/containers/GridCompareSection.js
@@ -89,11 +89,13 @@ const GridCompareSection = () => {
   // Example usage:
   useEffect(() => {
     if (previousData.length > 1) {
+      console.log(previousData);
       const report = getReportCompareGrid(previousData);
-      const heatMapData = getGridHeatMapData(previousData);
+      // const heatMapData = getGridHeatMapData(previousData);
 
+      console.log(JSON.stringify(report), "rekfk");
       setGridCompareData(report);
-      setHeatMapData(heatMapData);
+      // setHeatMapData(heatMapData);
     }
   }, [customData, previousData]);
   const classes = useStyles();
@@ -132,9 +134,9 @@ const GridCompareSection = () => {
       {previousData && data && (
         <GridSCompare gridCompareData={gridCompareData} />
       )}
-      {previousData && heatMapData && (
+      {/* {previousData && heatMapData && (
         <GridHeatmap gridCompareData={heatMapData} />
-      )}{" "}
+      )}{" "} */}
       {/* <GridHeatmap /> */}
     </Box>
   );
--- src/utils/getReportCompareGrid.js
+++ src/utils/getReportCompareGrid.js
@@ -1,39 +1,73 @@
 import { NanosecondsConverter } from "./nanosecondConverter";
 
+// export function getReportCompareGrid(data) {
+//   let formattedRows = [];
+//   let serialCounter = 1;
+//   for (const entry of data) {
+//     // Iterate over each data entry within the datetime
+//     for (const rowData of entry.data) {
+//       const id = rowData.featureId;
+//       const name = rowData.featureName;
+//       const reportName = rowData?.reportName;
+
+//       const duration =
+//         rowData.details.length > 0
+//           ? rowData.details.reduce((acc, curr) => {
+//               const durationValue = parseFloat(curr.stepResultDuration);
+//               return isNaN(durationValue) ? acc : acc + durationValue;
+//             }, 0)
+//           : 0;
+
+//       let row = formattedRows.find((item) => item.id === id);
+
+//       // If the row for the ID does not exist, create it
+//       if (!row) {
+//         row = {
+//           id: id,
+//           Id: serialCounter++,
+//           Feature_Name: name,
+//         };
+//         // Assign the value for the current date
+
+//         formattedRows.push(row);
+//       }
+
+//       // Assign the value for the current date
+//       row[`${reportName}-${entry.datetime}-duration`] =
+//         NanosecondsConverter(duration).totalDuration;
+//     }
+//   }
+
+//   return formattedRows;
+// }
+
 export function getReportCompareGrid(data) {
-  let formattedRows = [];
+  const formattedRows = [];
   let serialCounter = 1;
-  for (const entry of data) {
-    // Iterate over each data entry within the datetime
-    for (const rowData of entry.data) {
-      const id = rowData.featureId;
-      const name = rowData.featureName;
-      const reportName = rowData?.reportName;
+  data.forEach((entry) => {
+    entry.data.forEach((reportData) => {
+      let row = formattedRows.find((row) => row.id === reportData.featureId);
+      if (!row) {
+        row = {
+          id: reportData.featureId,
+          Id: serialCounter++,
+          Feature_Name: reportData.featureName,
+        };
+        formattedRows.push(row);
+      }
 
       const duration =
-        rowData.details.length > 0
-          ? rowData.details.reduce((acc, curr) => {
+        reportData.details.length > 0
+          ? reportData.details.reduce((acc, curr) => {
               const durationValue = parseFloat(curr.stepResultDuration);
               return isNaN(durationValue) ? acc : acc + durationValue;
             }, 0)
           : 0;
 
-      let row = formattedRows.find((item) => item.id === id);
-
-      // If the row for the ID does not exist, create it
-      if (!row) {
-        row = {
-          id: id,
-          Id: serialCounter++,
-          Feature_Name: name,
-        };
-        formattedRows.push(row);
-      }
-
-      // Assign the value for the current date
-      row[`${reportName}-${entry.datetime}-duration`] =
-        NanosecondsConverter(duration).totalDuration;
-    }
-  }
+      // Unique key for each report's duration based on its name and the entry datetime
+      const durationKey = `${reportData.reportName}-${entry.datetime}-duration`;
+      row[durationKey] = NanosecondsConverter(duration).totalDuration;
+    });
+  });
   return formattedRows;
 }
